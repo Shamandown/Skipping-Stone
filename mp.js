5e86e6eb60e06c8727072f7e3e16514ffc35bde9
Eric Landreneau <44383203+Shamandown@users.noreply.github.com>
2020-08-30 22:21:56 -0700

    Built up lotsa functions!

--- mp.js
+++ mp.js
@@ -245,7 +245,7 @@ function icedearth(){
 			document.getElementById("icedEarthChoice").setAttribute("onClick","");
 			document.getElementById("hotEarthChoice").style.backgroundColor="black";
 			document.getElementById("hotEarthChoice").setAttribute("onClick","");
-			document.body.style.backgroundImage = "url(images/BGPH-icedearth)";
+			document.body.style.backgroundImage = "url(images/BGPH-icedearth.jpg)";
 		}
 		else{
 			icedearth.style.display = "none";
@@ -254,6 +254,176 @@ function icedearth(){
 			document.getElementById("hotEarthChoice").style.backgroundColor="";
 			document.getElementById("hotEarthChoice").setAttribute("onClick","outlaw()");
 			document.body.style.backgroundImage = "url(images/BGPH-barbell.jpg)";
+		}
+}
+
+// third tier of divergent choices
+
+function enabler(){
+	var betrayer = document.getElementById("betrayer");
+	var enabler = document.getElementById("enabler");
+		if (betrayer.style.display == "none" && enabler.style.display == "none"){
+			enabler.style.display = "block";
+			document.getElementById("betrayerChoice").style.backgroundColor="black";
+			document.getElementById("betrayerChoice").setAttribute("onClick","");
+			document.getElementById("enablerChoice").style.backgroundColor="black";
+			document.getElementById("enablerChoice").setAttribute("onClick","");
+			document.body.style.backgroundImage = "url(images/BGPH-enabler.jpg)";
+		}
+		else{
+			enabler.style.display = "none";
+			document.getElementById("betrayerChoice").style.backgroundColor="";
+			document.getElementById("betrayerChoice").setAttribute("onClick","betrayer()");
+			document.getElementById("enablerChoice").style.backgroundColor="";
+			document.getElementById("enablerChoice").setAttribute("onClick","enabler()");
+			document.body.style.backgroundImage = "url(images/BGPH-madscience.jpg)";
+		}
+}
+
+function betrayer(){
+	var betrayer = document.getElementById("betrayer");
+	var enabler = document.getElementById("enabler");
+		if (betrayer.style.display == "none" && enabler.style.display == "none"){
+			betrayer.style.display = "block";
+			document.getElementById("betrayerChoice").style.backgroundColor="black";
+			document.getElementById("betrayerChoice").setAttribute("onClick","");
+			document.getElementById("enablerChoice").style.backgroundColor="black";
+			document.getElementById("enablerChoice").setAttribute("onClick","");
+			document.body.style.backgroundImage = "url(images/BGPH-betrayer.jpg)";
+		}
+		else{
+			betrayer.style.display = "none";
+			document.getElementById("betrayerChoice").style.backgroundColor="";
+			document.getElementById("betrayerChoice").setAttribute("onClick","betrayer()");
+			document.getElementById("enablerChoice").style.backgroundColor="";
+			document.getElementById("enablerChoice").setAttribute("onClick","enabler()");
+			document.body.style.backgroundImage = "url(images/BGPH-madscience.jpg)";
+		}
+}
+
+function noMatterWhat(){
+	var loveAndHope = document.getElementById("loveAndHope");
+	var noMatterWhat = document.getElementById("noMatterWhat");
+		if (loveAndHope.style.display == "none" && noMatterWhat.style.display == "none"){
+			noMatterWhat.style.display = "block";
+			document.getElementById("chooseLoveAndHope").style.backgroundColor="black";
+			document.getElementById("chooseLoveAndHope").setAttribute("onClick","");
+			document.getElementById("noMatterChoice").style.backgroundColor="black";
+			document.getElementById("noMatterChoice").setAttribute("onClick","");
+			document.body.style.backgroundImage = "url(images/BGPH-nomatter.jpg)";
+		}
+		else{
+			noMatterWhat.style.display = "none";
+			document.getElementById("chooseLoveAndHope").style.backgroundColor="";
+			document.getElementById("chooseLoveAndHope").setAttribute("onClick","loveAndHope()");
+			document.getElementById("noMatterChoice").style.backgroundColor="";
+			document.getElementById("noMatterChoice").setAttribute("onClick","noMatterWhat()");
+			document.body.style.backgroundImage = "url(images/BGPH-monsterhunter.jpg)";
+		}
+}
+
+function loveAndHope(){
+	var loveAndHope = document.getElementById("loveAndHope");
+	var noMatterWhat = document.getElementById("noMatterWhat");
+		if (loveAndHope.style.display == "none" && noMatterWhat.style.display == "none"){
+			loveAndHope.style.display = "block";
+			document.getElementById("chooseLoveAndHope").style.backgroundColor="black";
+			document.getElementById("chooseLoveAndHope").setAttribute("onClick","");
+			document.getElementById("noMatterChoice").style.backgroundColor="black";
+			document.getElementById("noMatterChoice").setAttribute("onClick","");
+			document.body.style.backgroundImage = "url(images/BGPH-loveandhope.jpg)";
+		}
+		else{
+			loveAndHope.style.display = "none";
+			document.getElementById("chooseLoveAndHope").style.backgroundColor="";
+			document.getElementById("chooseLoveAndHope").setAttribute("onClick","loveAndHope()");
+			document.getElementById("noMatterChoice").style.backgroundColor="";
+			document.getElementById("noMatterChoice").setAttribute("onClick","noMatterWhat()");
+			document.body.style.backgroundImage = "url(images/BGPH-monsterhunter.jpg)";
+		}
+}
+
+function stayFocused(){
+	var discover = document.getElementById("discovery");
+	var focused = document.getElementById("stayFocused");
+		if (discover.style.display == "none" && focused.style.display == "none"){
+			focused.style.display = "block";
+			document.getElementById("discoveryChoice").style.backgroundColor="black";
+			document.getElementById("discoveryChoice").setAttribute("onClick","");
+			document.getElementById("focusChoice").style.backgroundColor="black";
+			document.getElementById("focusChoice").setAttribute("onClick","");
+			document.body.style.backgroundImage = "url(images/BGPH-focus.jpg)";
+		}
+		else{
+			focused.style.display = "none";
+			document.getElementById("discoveryChoice").style.backgroundColor="";
+			document.getElementById("discoveryChoice").setAttribute("onClick","restorefunc()");
+			document.getElementById("focusChoice").style.backgroundColor="";
+			document.getElementById("focusChoice").setAttribute("onClick","restorefunc()");
+			document.body.style.backgroundImage = "url(images/BGPH-icedearth.jpg)";
+		}
+}
+
+function discover(){
+	var discover = document.getElementById("discovery");
+	var focused = document.getElementById("stayFocused");
+		if (discover.style.display == "none" && focused.style.display == "none"){
+			discover.style.display = "block";
+			document.getElementById("discoveryChoice").style.backgroundColor="black";
+			document.getElementById("discoveryChoice").setAttribute("onClick","");
+			document.getElementById("focusChoice").style.backgroundColor="black";
+			document.getElementById("focusChoice").setAttribute("onClick","");
+			document.body.style.backgroundImage = "url(images/BGPH-discovery.jpg)";
+		}
+		else{
+			discover.style.display = "none";
+			document.getElementById("discoveryChoice").style.backgroundColor="";
+			document.getElementById("discoveryChoice").setAttribute("onClick","restorefunc()");
+			document.getElementById("focusChoice").style.backgroundColor="";
+			document.getElementById("focusChoice").setAttribute("onClick","restorefunc()");
+			document.body.style.backgroundImage = "url(images/BGPH-icedearth.jpg)";
+		}
+}
+
+function trust(){
+	var stopHim = document.getElementById("stopHim");
+	var trustHim = document.getElementById("trust");
+		if (stopHim.style.display == "none" && trustHim.style.display == "none"){
+			trustHim.style.display = "block";
+			document.getElementById("stopChoice").style.backgroundColor="black";
+			document.getElementById("stopChoice").setAttribute("onClick","");
+			document.getElementById("chooseTrust").style.backgroundColor="black";
+			document.getElementById("chooseTrust").setAttribute("onClick","");
+			document.body.style.backgroundImage = "url(images/BGPH-Trust.jpg)";
+		}
+		else{
+			trustHim.style.display = "none";
+			document.getElementById("stopChoice").style.backgroundColor="";
+			document.getElementById("stopChoice").setAttribute("onClick","stopHim()");
+			document.getElementById("chooseTrust").style.backgroundColor="";
+			document.getElementById("chooseTrust").setAttribute("onClick","trust()");
+			document.body.style.backgroundImage = "url(images/BGPH-outlaw.jpg)";
+		}
+}
+
+function stopHim(){
+	var stopHim = document.getElementById("stopHim");
+	var trustHim = document.getElementById("trust");
+		if (stopHim.style.display == "none" && trustHim.style.display == "none"){
+			stopHim.style.display = "block";
+			document.getElementById("stopChoice").style.backgroundColor="black";
+			document.getElementById("stopChoice").setAttribute("onClick","");
+			document.getElementById("chooseTrust").style.backgroundColor="black";
+			document.getElementById("chooseTrust").setAttribute("onClick","");
+			document.body.style.backgroundImage = "url(images/BGPH-stophim.jpg)";
+		}
+		else{
+			stopHim.style.display = "none";
+			document.getElementById("stopChoice").style.backgroundColor="";
+			document.getElementById("stopChoice").setAttribute("onClick","stopHim()");
+			document.getElementById("chooseTrust").style.backgroundColor="";
+			document.getElementById("chooseTrust").setAttribute("onClick","trust()");
+			document.body.style.backgroundImage = "url(images/BGPH-outlaw.jpg)";
 		}
 }
 
@@ -286,27 +456,37 @@ This will get mo funky when I really start branching, and when branches ad more 
 function clearance(){
 	//Template: if(document.getElementById("tobecleared").style.display == "block"){clearcallfunc();}
 	setTimeout(function () {
-	if(document.getElementById("icedEarth").style.display == "block"){icedearth();}
-	if(document.getElementById("outlaw").style.display == "block"){outlaw();}
-	if(document.getElementById("madScience").style.display == "block"){madScience();}
-	if(document.getElementById("monsterHunter").style.display == "block"){monsterHunter();}
-	}, 0010);
+	if(document.getElementById("enabler").style.display == "block"){enabler();}
+	if(document.getElementById("betrayer").style.display == "block"){betrayer();}
+	if(document.getElementById("noMatterWhat").style.display == "block"){noMatterWhat();}
+	if(document.getElementById("loveAndHope").style.display == "block"){loveAndHope();}
+	if(document.getElementById("stayFocused").style.display == "block"){stayFocused();}
+	if(document.getElementById("discovery").style.display == "block"){discover();}
+	if(document.getElementById("trust").style.display == "block"){trust();}
+	if(document.getElementById("stopHim").style.display == "block"){stopHim();}
+	}, 0010);	
 		setTimeout(function () {
-		if(document.getElementById("torus").style.display == "block"){donut();}
-		if(document.getElementById("barbell").style.display == "block"){dumbell();}
+		if(document.getElementById("icedEarth").style.display == "block"){icedearth();}
+		if(document.getElementById("outlaw").style.display == "block"){outlaw();}
+		if(document.getElementById("madScience").style.display == "block"){madScience();}
+		if(document.getElementById("monsterHunter").style.display == "block"){monsterHunter();}
 		}, 1100);
 			setTimeout(function () {
-			if(document.getElementById("death3").style.display == "block"){death3();}
-			if(document.getElementById("upperRoom").style.display == "block"){upperRoom();}
+			if(document.getElementById("torus").style.display == "block"){donut();}
+			if(document.getElementById("barbell").style.display == "block"){dumbell();}
 			}, 2200);
-				setTimeout(function () { //breakdown here: bgimage blacks out on revert, rather than animates smoothly. no likey
-				if(document.getElementById("enter").style.display == "block"){enter();}
-				if(document.getElementById("death2").style.display == "block"){death2();}
+				setTimeout(function () {
+				if(document.getElementById("death3").style.display == "block"){death3();}
+				if(document.getElementById("upperRoom").style.display == "block"){upperRoom();}
 				}, 3300);
-					setTimeout(function () {
-					if(document.getElementById("adrift").style.display == "block"){adrift();}
-					if(document.getElementById("landing").style.display == "block"){landing();}
+					setTimeout(function () { //breakdown here: bgimage blacks out on revert, rather than animates smoothly. no likey
+					if(document.getElementById("enter").style.display == "block"){enter();}
+					if(document.getElementById("death2").style.display == "block"){death2();}
 					}, 4400);
+						setTimeout(function () {
+						if(document.getElementById("adrift").style.display == "block"){adrift();}
+						if(document.getElementById("landing").style.display == "block"){landing();}
+						}, 5500);
 }
 
 	/* Set the width of the side navigation to 250px */
